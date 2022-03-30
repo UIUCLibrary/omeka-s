9a350ced9a5504afe6a10a022eee45d11dd706b8
Alex Dryden <adryden3@illinois.edu>
2022-03-30 12:56:45 -0400

    Use vanilla js to get user id

--- application/asset/js/matomo.js
+++ application/asset/js/matomo.js
@@ -2,7 +2,7 @@
     var _paq = window._paq = window._paq || [];
     /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
     /* get user id from last segment of the link to user profile  */
-    let userID = $('.user-id:first-of-type a').attr('href').split( '/' ).slice(-1)
+    let userID = document.querySelector(".user-id:first-of-type a").getAttribute("href").split( '/' ).slice(-1)[0]
     _paq.push(['setUserId', userID]);
     _paq.push(['trackPageView']);
     _paq.push(['enableLinkTracking']);
